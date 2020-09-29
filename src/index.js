439ad02260c3e3c92db5bad3a116162694be011a
Andrey Lisovskiy <andrey.lisovskiy@intrahealth.com>
2020-09-29 15:22:32 +0300

    feat: implement brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let config = new Map(bracketsConfig);
+    let stack = [];
+    for (let c of str.split('')) {
+        if (stack.length && c === config.get(stack[stack.length - 1])) {
+            stack.pop();
+        } else if (config.has(c)) {
+            stack.push(c)
+        } else {
+            if (c !== config.get(stack.pop())) return false;
+        }
+    }
+    return stack.length === 0;
 }
